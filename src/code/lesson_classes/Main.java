5e7b8b5365293a0579b143c0ab9ef0c8c420c160
Андрей <andreylebedinec2001@gmail.com>
2022-02-05 21:05:32 +0300

    Add MessagePrinter.java

--- src/code/lesson_classes/Main.java
+++ src/code/lesson_classes/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Hello");
-
-
+        MessagePrinter messagePrinter = new MessagePrinter("Cat");
+        messagePrinter.print();
 
     }
 
---
+++ src/code/lesson_classes/MessagePrinter.java
@@ -0,0 +1,21 @@
+package code.lesson_classes;
+
+public class MessagePrinter {
+
+    private String variable;
+
+    public MessagePrinter(String variable){
+
+        this.variable = variable;
+
+    }
+
+    public void print(){
+
+        System.out.println(variable);
+
+    }
+
+
+
+}
