4314dcf90b517edafbb5fc57cc02d8e6a4d8b467
Андрей <andreylebedinec2001@gmail.com>
2022-02-13 15:24:01 +0300

    Add

---
+++ src/code/hometasks/Auxiliary.java
@@ -0,0 +1,20 @@
+package code.hometasks;
+
+public class Auxiliary {
+    public double t1;
+    public double t2;
+    public double t3;
+
+    Auxiliary(double t1,double t2){
+        this.t1 = t1;
+        this.t2 = t2;
+
+    }
+    Auxiliary(){};
+    Phone j = new Phone();
+
+
+
+
+
+}
